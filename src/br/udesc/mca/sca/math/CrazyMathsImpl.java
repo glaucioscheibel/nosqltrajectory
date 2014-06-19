06011198ce966a34d945c6240e5e210b370a5ede
Glaucio Scheibel <glaucioscheibel@users.noreply.github.com>
2014-06-19 22:49:44 +0000

    Criado o composite CrazyMaths

--- src/br/udesc/mca/sca/math/CrazyMathsImpl.java
+++ src/br/udesc/mca/sca/math/CrazyMathsImpl.java
@@ -1,5 +1,9 @@
 package br.udesc.mca.sca.math;
 
-public class CrazyMathsImpl {
+public class CrazyMathsImpl implements CrazyMaths {
 
+    @Override
+    public double add(double d1, double d2) {
+        return d1 + d2;
+    }
 }

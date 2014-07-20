79ebfd7ab8f11b9fa9a8fcd35297ed9e51b5ebfc
Glaucio Scheibel <glaucioscheibel@users.noreply.github.com>
2014-07-20 17:07:15 +0000

    Mavenização do projeto

--- src/br/udesc/mca/sec1/projeto/dao/DBCloser.java
+++
@@ -1,9 +0,0 @@
-package br.udesc.mca.sec1.projeto.dao;
-
-public class DBCloser extends Thread {
-
-    @Override
-    public void run() {
-        PersistenceAbstractFactory.closeAll();
-    }
-}
--- src/br/udesc/mca/sec1/projeto/dao/PersistenceModel.java
+++
@@ -1,5 +0,0 @@
-package br.udesc.mca.sec1.projeto.dao;
-
-public enum PersistenceModel {
-    DOCUMENT, KEY_VALUE, RELATIONAL, COLUMN_FAMILY, GRAPH;
-}

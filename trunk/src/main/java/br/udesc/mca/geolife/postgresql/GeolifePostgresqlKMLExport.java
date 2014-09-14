e37c4283814178308146d55bea203c76314bc044
gscheibel <gscheibel@6027d77a-0421-43a9-82e4-94cb8f093177>
2014-09-14 19:47:13 +0000

    Acertado o exportador de kml para o novo formato.

--- trunk/src/main/java/br/udesc/mca/geolife/postgresql/GeolifePostgresqlKMLExport.java
+++ trunk/src/main/java/br/udesc/mca/geolife/postgresql/GeolifePostgresqlKMLExport.java
@@ -7,8 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.List;
 
-import org.postgresql.geometric.PGpoint;
-
 import de.micromata.opengis.kml.v_2_2_0.Coordinate;
 import de.micromata.opengis.kml.v_2_2_0.Kml;
 import de.micromata.opengis.kml.v_2_2_0.LineString;
@@ -18,25 +16,23 @@ import de.micromata.opengis.kml.v_2_2_0.Placemark;
 public class GeolifePostgresqlKMLExport {
     public static void main(String[] args) throws Exception {
         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost/trajectories", "postgres", "admin");
-        PreparedStatement ps = con.prepareStatement("Select latlon from geolife where id=? and trajid=?");
-        int id = 0;
+        PreparedStatement ps = con.prepareStatement("Select longitude, latitude from geolife where userid=? and trajid=?");
+        int userid = 0;
         long[] trajids = {20081023025304L, 20081026134407L};
         Kml kml = new Kml();
-        
+
         Placemark p = kml.createAndSetPlacemark();
         p.setDescription("Trajectories");
         MultiGeometry mg = p.createAndSetMultiGeometry();
-        
+
         for (long trajid : trajids) {
-            
             LineString ls = mg.createAndAddLineString();
             List<Coordinate> lc = ls.createAndSetCoordinates();
-            ps.setInt(1, id);
+            ps.setInt(1, userid);
             ps.setLong(2, trajid);
             ResultSet rs = ps.executeQuery();
             while (rs.next()) {
-                PGpoint point = (PGpoint) rs.getObject(1);
-                lc.add(new Coordinate(point.y, point.x));
+                lc.add(new Coordinate(rs.getDouble(1), rs.getDouble(2)));
             }
             rs.close();
         }

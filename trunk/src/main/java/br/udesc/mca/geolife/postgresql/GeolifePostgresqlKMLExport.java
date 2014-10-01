89e98a925615f6c89132c179c1c6e84868ad7204
gscheibel <gscheibel@6027d77a-0421-43a9-82e4-94cb8f093177>
2014-10-01 23:02:49 +0000

    Exporta todos as trajetorias do usuário 128

--- trunk/src/main/java/br/udesc/mca/geolife/postgresql/GeolifePostgresqlKMLExport.java
+++ trunk/src/main/java/br/udesc/mca/geolife/postgresql/GeolifePostgresqlKMLExport.java
@@ -5,6 +5,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
 
 import de.micromata.opengis.kml.v_2_2_0.Coordinate;
@@ -15,7 +16,7 @@ import de.micromata.opengis.kml.v_2_2_0.Placemark;
 
 public class GeolifePostgresqlKMLExport {
     
-    public void export(int userId, long[] trajIds, File kmlFile) throws Exception {
+    public void export(int userId, Long[] trajIds, File kmlFile) throws Exception {
         Connection con = DriverManager.getConnection("jdbc:postgresql://localhost/trajectories", "postgres", "admin");
         PreparedStatement ps = con.prepareStatement("Select longitude, latitude from geolife where userid=? and trajid=?");
         Kml kml = new Kml();
@@ -24,7 +25,7 @@ public class GeolifePostgresqlKMLExport {
         p.setDescription("Trajectories");
         MultiGeometry mg = p.createAndSetMultiGeometry();
 
-        for (long trajId : trajIds) {
+        for (Long trajId : trajIds) {
             LineString ls = mg.createAndAddLineString();
             List<Coordinate> lc = ls.createAndSetCoordinates();
             ps.setInt(1, userId);
@@ -40,11 +41,26 @@ public class GeolifePostgresqlKMLExport {
         kml.marshal(kmlFile);
     }
     
+    public void exportByUser(int userId, File kmlFile) throws Exception {
+        List<Long> ll = new ArrayList<>();
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost/trajectories", "postgres", "admin");
+        PreparedStatement ps = con.prepareStatement("Select distinct trajid from geolife where userid=? order by trajid");
+        ps.setInt(1, userId);
+        ResultSet rs = ps.executeQuery();
+        while (rs.next()) {
+            ll.add(rs.getLong(1));
+        }
+        rs.close();
+        ps.close();
+        con.close();
+        this.export(userId, ll.toArray(new Long[0]), kmlFile);
+    }
+    
     public static void main(String[] args) throws Exception {
-        int userId = 0;
-        long[] trajIds = {20081023025304L, 20081026134407L};
+        int userId = 128;
+        //Long[] trajIds = {20081023025304L, 20081026134407L};
         File kmlFile = new File("teste.kml"); 
         GeolifePostgresqlKMLExport exp = new GeolifePostgresqlKMLExport();
-        exp.export(userId, trajIds, kmlFile);
+        exp.exportByUser(userId, kmlFile);
     }
 }

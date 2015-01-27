90ea0f77204ff116a2d226d061f02e0698f7077f
fhofmann01 <fhofmanns@users.noreply.github.com>
2015-01-27 16:07:51 +0000

    First Python/Jython test

--- src/main/java/br/udesc/mca/HelloWord.java
+++ src/main/java/br/udesc/mca/HelloWord.java
@@ -1,10 +1,20 @@
 package br.udesc.mca;
 
+import org.python.util.PythonInterpreter;
+
 public class HelloWord {
 
     public static void main(String[] args) {
         System.out.println("Crazy Maths");
         System.out.println("Crazy Maths!");
+
+        //http://www.jython.org/jythonbook/en/1.0/JythonAndJavaIntegration.html
+        try {
+            PythonInterpreter py = new PythonInterpreter();
+            py.exec("print \"From Python: Crazy Maths\"");
+        }catch (Exception e){
+            e.printStackTrace();
+        }
 
     }
 

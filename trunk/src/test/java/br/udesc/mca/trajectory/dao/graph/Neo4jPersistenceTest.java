6606eda183c353b4cb49a7fedda6c417f10604d9
gscheibel <gscheibel@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-02-01 21:35:00 +0000

    Refactor do pacote de testes unitários

--- trunk/src/test/java/br/udesc/mca/trajectory/dao/graph/Neo4jPersistenceTest.java
+++ trunk/src/test/java/br/udesc/mca/trajectory/dao/graph/Neo4jPersistenceTest.java
@@ -1,13 +1,12 @@
-package br.udesc.mca.sec1.projeto.dao.graph;
+package br.udesc.mca.trajectory.dao.graph;
 
 import java.io.File;
 
 import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
 
-import br.udesc.mca.sec1.projeto.dao.PersistenceTest;
 import br.udesc.mca.trajectory.dao.PersistenceModel;
-import br.udesc.mca.trajectory.dao.graph.Neo4jPersistence;
+import br.udesc.mca.trajectory.dao.PersistenceTest;
 import br.udesc.mca.trajectory.model.Trajectory;
 
 public class Neo4jPersistenceTest extends PersistenceTest {

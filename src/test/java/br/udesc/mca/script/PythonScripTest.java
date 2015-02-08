c0d90241950af6944bafe67dcb9e4ef4f46f06bf
fhofmann01 <fhofmanns@users.noreply.github.com>
2015-02-08 19:24:16 +0000

    Implementacao inicial do engine de script com python

---
+++ src/test/java/br/udesc/mca/script/PythonScripTest.java
@@ -0,0 +1,37 @@
+package br.udesc.mca.script;
+
+import br.udesc.mca.trajectory.model.TrajectoryPoint;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertTrue;
+
+public class PythonScripTest {
+
+    @Test
+    public void testInicial() {
+        ScriptEngine engine = ScriptEngine.initEngine("python");
+        engine.setExecutionContext("processListTest");//nome do arquivo .py  (OBS: tem que estar no path da execucao do java)
+
+
+        List<TrajectoryPoint> pl = new ArrayList<>();
+        TrajectoryPoint p = new TrajectoryPoint();
+        p.setY(80.0f);
+        p.setX(80.0f);
+        p.setTimestamp(System.currentTimeMillis());
+        pl.add(p);
+
+        List<TrajectoryPoint> rpl = null;
+        try {
+            rpl = engine.processTrajectoryList(pl);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw  new RuntimeException(e);
+        }
+
+
+        assertTrue(rpl.get(0).getY() == 79.0f);
+    }
+}
---
+++ src/test/resources/processListTest.py
@@ -0,0 +1,10 @@
+listToProcess = [];
+listToReturn = [];
+
+def processList(self):
+    print 'metodo processList'
+    print listToProcess
+    self.listToReturn =   [[listToProcess[0][0] -1,1.2,2014,10,10,14,34,36,.9]];
+
+if __name__ == '__main__':
+    processList()

7740c32b0f70a879d717872d6bf71c50bb7ebae6
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-04-19 16:07:37 +0000

    - Conversor de ponto e teste do azimute.

--- trunk/src/main/java/br/udesc/mca/azimuth/TesteAzimuth.java
+++ trunk/src/main/java/br/udesc/mca/azimuth/TesteAzimuth.java
@@ -1,5 +1,7 @@
 package br.udesc.mca.azimuth;
 
+import br.udesc.mca.point.converter.PointConverter;
+
 public class TesteAzimuth {
 
 	public static void main(String[] args) {
@@ -7,6 +9,9 @@ public class TesteAzimuth {
 		GeoCoordinate udesc2 = new GeoCoordinate(-26.1509445, -48.8814309);
 		// GeoCoordinate udesc = new GeoCoordinate(35, 45);
 		// GeoCoordinate udesc2 = new GeoCoordinate(35, 135);
+		
+		//PointConverter.converterDecimalDegress("26°15'23.35\"s");
+		PointConverter.converterDecimalDegress(" -26°15'23.35\"S");
 
 		double distancia = Azimuth.calculateLengthInKM(udesc.getLatitude(),
 				udesc.getLongitude(), udesc2.getLatitude(),

02ba4f4c7bf1b8630579fea4d2b86c8c73297594
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-04-19 23:11:34 +0000

    Teste conversão de pontos

--- trunk/src/main/java/br/udesc/mca/azimuth/TesteAzimuth.java
+++ trunk/src/main/java/br/udesc/mca/azimuth/TesteAzimuth.java
@@ -13,32 +13,75 @@ public class TesteAzimuth {
 				.converterDecimalDegress("26°15'23.35\"S"));
 		System.out.println(PointConverter
 				.converterDecimalDegress("48°54'20.59\"O"));
-
+		System.out.println();
 		System.out.println(PointConverter
 				.converterDecimalDegress("26°15'22.88\"S"));
 		System.out.println(PointConverter
 				.converterDecimalDegress("48°54'21.41\"O"));
-
+		System.out.println();
 		System.out.println(PointConverter
 				.converterDecimalDegress("26°15'22.06\"S"));
 		System.out.println(PointConverter
 				.converterDecimalDegress("48°54'21.81\"O"));
-
+		System.out.println();
 		System.out.println(PointConverter
 				.converterDecimalDegress("26°15'21.07\"S"));
 		System.out.println(PointConverter
 				.converterDecimalDegress("48°54'21.76\"O"));
-
+		System.out.println();
 		System.out.println(PointConverter
 				.converterDecimalDegress("26°15'20.21\"S"));
 		System.out.println(PointConverter
 				.converterDecimalDegress("48°54'20.96\"O"));
-
+		System.out.println();
 		System.out.println(PointConverter
 				.converterDecimalDegress("26°15'20.06\"S"));
 		System.out.println(PointConverter
 				.converterDecimalDegress("48°54'19.61\"O"));
+		System.out.println();
+		System.out.println(PointConverter
+				.converterDecimalDegress("26°15'21.44\"S"));
+		System.out.println(PointConverter
+				.converterDecimalDegress("48°54'18.11\"O"));
 
+
+		System.out.println("=================================================");
+		System.out.println(PointConverter
+				.converterDecimalDegress("26°15'24.00\"S"));
+		System.out.println(PointConverter
+				.converterDecimalDegress("48°54'14.17\"O"));
+		System.out.println();
+		System.out.println(PointConverter
+				.converterDecimalDegress("26°15'24.51\"S"));
+		System.out.println(PointConverter
+				.converterDecimalDegress("48°54'13.05\"O"));
+		System.out.println();
+		System.out.println(PointConverter
+				.converterDecimalDegress("26°15'25.33\"S"));
+		System.out.println(PointConverter
+				.converterDecimalDegress("48°54'12.56\"O"));
+		System.out.println();
+		System.out.println(PointConverter
+				.converterDecimalDegress("26°15'26.32\"S"));
+		System.out.println(PointConverter
+				.converterDecimalDegress("48°54'12.71\"O"));
+		System.out.println();
+		System.out.println(PointConverter
+				.converterDecimalDegress("26°15'26.92\"S"));
+		System.out.println(PointConverter
+				.converterDecimalDegress("48°54'13.34\"O"));
+		System.out.println();
+		System.out.println(PointConverter
+				.converterDecimalDegress("26°15'26.91\"S"));
+		System.out.println(PointConverter
+				.converterDecimalDegress("48°54'14.87\"O"));
+		System.out.println();
+		System.out.println(PointConverter
+				.converterDecimalDegress("26°15'25.63\"S"));
+		System.out.println(PointConverter
+				.converterDecimalDegress("48°54'15.93\"O"));
+		System.out.println();
+		
 		/*
 		 * double distancia = Azimuth.calculateLengthInKM(udesc.getLatitude(),
 		 * udesc.getLongitude(), udesc2.getLatitude(), udesc2.getLongitude());

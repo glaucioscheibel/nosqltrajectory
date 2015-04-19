049aaad107e0ea1ca1b6ea7fa5bf3d2ded4f94ff
gscheibel <gscheibel@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-04-19 22:14:12 +0000

    Remoção de dependência do apache tuscany

--- trunk/src/main/java/br/udesc/mca/trajectory/service/TrajectoryInput.java
+++ trunk/src/main/java/br/udesc/mca/trajectory/service/TrajectoryInput.java
@@ -1,22 +1,21 @@
 package br.udesc.mca.trajectory.service;
 
-
-import org.oasisopen.sca.annotation.Remotable;
-
-
 /**
- * Servico para inserir trajetorias das mais diversas formas nos diferentes modos de persistencia
+ * Servico para inserir trajetorias das mais diversas formas nos diferentes
+ * modos de persistencia
  */
-@Remotable
 public interface TrajectoryInput {
 
     /**
-     * Inclui um ponto em uma nova trajetoria e retorna o ID da trajetoria criada
+     * Inclui um ponto em uma nova trajetoria e retorna o ID da trajetoria
+     * criada
      */
-    public long insertSinglePointWithDateTime(float latitude, float longitude, int ano, int mes, int dia, int hora, int min, int seg, String descricao);
+    public long insertSinglePointWithDateTime(float latitude, float longitude, int ano, int mes, int dia, int hora,
+            int min, int seg, String descricao);
 
     /**
-     * Iclui um novo ponto em uma trajetoria usando a data/hora atual. Para uma nova trajetoria informar  um id < 1 para <code>idTrajetoria</code>
+     * Iclui um novo ponto em uma trajetoria usando a data/hora atual. Para uma
+     * nova trajetoria informar um id < 1 para <code>idTrajetoria</code>
      */
     public long insertSinglePoint(long idTrajetoria, float latitude, float longitude);
 }

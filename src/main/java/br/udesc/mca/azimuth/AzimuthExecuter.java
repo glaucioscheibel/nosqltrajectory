791509c3f19fbdcb6c085ab0425be5c1bec79f32
Glaucio Scheibel <glaucioscheibel@users.noreply.github.com>
2015-04-25 18:09:12 +0000

    Calculo de diferença de azimuths

--- src/main/java/br/udesc/mca/azimuth/AzimuthExecuter.java
+++ src/main/java/br/udesc/mca/azimuth/AzimuthExecuter.java
@@ -15,16 +15,27 @@ public class AzimuthExecuter {
                 .getInstance(PersistenceModel.RELATIONAL);
         Trajectory t = dao.findById(20150418093055L);
         TrajectoryVersion tv = t.getVersions().get(1);
+        double aziAnt = 0D;
+        boolean first = true;
         for (TrajectorySegment ts : tv.getSegments()) {
             float lat1 = ts.getPoints().get(0).getLat();
             float lgn1 = ts.getPoints().get(0).getLng();
             float lat2 = ts.getPoints().get(1).getLat();
             float lgn2 = ts.getPoints().get(1).getLng();
             double azi = Azimuth.azimuth(lat1, lgn1, lat2, lgn2);
+            if (first) {
+                aziAnt = azi;
+                first = false;
+            }
             TrajectorySegmentData tsd = new TrajectorySegmentData();
             tsd.setKey("azimuth");
             tsd.setValue(String.valueOf(azi));
             ts.addData(tsd);
+            TrajectorySegmentData tsd2 = new TrajectorySegmentData();
+            tsd2.setKey("azimuthDiff");
+            tsd2.setValue(String.valueOf(azi - aziAnt));
+            ts.addData(tsd2);
+            aziAnt = azi;
         }
         dao.store(t);
     }

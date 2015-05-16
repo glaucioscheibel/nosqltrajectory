ef4e5bb09929024b9fdeac4276dbd72bc0b946a4
gscheibel <gscheibel@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-05-16 19:35:12 +0000

    A informação de diferença de azimuth passa para a trajetoria anterior

--- trunk/src/main/java/br/udesc/mca/azimuth/AzimuthExecuter.java
+++ trunk/src/main/java/br/udesc/mca/azimuth/AzimuthExecuter.java
@@ -13,7 +13,7 @@ public class AzimuthExecuter {
     public static void main(String[] args) throws Exception {
         PersistenceDAO<Trajectory> dao = (PersistenceDAO<Trajectory>) PersistenceDAO
                 .getInstance(PersistenceModel.RELATIONAL);
-        Trajectory t = dao.findById(20150419093055L);
+        Trajectory t = dao.findById(20150418093055L);
         TrajectoryVersion tv = t.getVersions().get(1);
         double aziAnt = 0D;
         TrajectorySegment tsAnt = null;
@@ -24,18 +24,20 @@ public class AzimuthExecuter {
             float lat2 = ts.getPoints().get(1).getLat();
             float lgn2 = ts.getPoints().get(1).getLng();
             double azi = Azimuth.azimuth(lat1, lgn1, lat2, lgn2);
-            if (first) {
-                tsAnt = ts;
-                aziAnt = azi;
-            }
             TrajectorySegmentData tsd = new TrajectorySegmentData();
             tsd.setKey("azimuth");
             tsd.setValue(String.valueOf(azi));
             ts.addData(tsd);
-            TrajectorySegmentData tsd2 = new TrajectorySegmentData();
-            tsd2.setKey("azimuthDiff");
-            tsd2.setValue(String.valueOf(Azimuth.azimuthDifference(azi, aziAnt)));
-            tsAnt.addData(tsd2);
+            if (!first) {
+                TrajectorySegmentData tsd2 = new TrajectorySegmentData();
+                tsd2.setKey("azimuthDiff");
+                tsd2.setValue(String.valueOf(Azimuth.azimuthDifference(azi, aziAnt)));
+                tsAnt.addData(tsd2);
+                aziAnt = azi;
+            } else {
+                first = false;
+            }
+            tsAnt = ts;
             aziAnt = azi;
         }
         dao.store(t);

e2e6580e064dbfc0c4f7b91d8cfd20f9d0588d57
gscheibel <gscheibel@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-05-16 20:01:43 +0000

    A informação de diferença de azimuth passa para a trajetoria anterior

--- trunk/src/main/java/br/udesc/mca/azimuth/AzimuthExecuter.java
+++ trunk/src/main/java/br/udesc/mca/azimuth/AzimuthExecuter.java
@@ -11,35 +11,38 @@ public class AzimuthExecuter {
 
     @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception {
+        long[] ll = {20150418093055L, 20150419093055L};
         PersistenceDAO<Trajectory> dao = (PersistenceDAO<Trajectory>) PersistenceDAO
                 .getInstance(PersistenceModel.RELATIONAL);
-        Trajectory t = dao.findById(20150418093055L);
-        TrajectoryVersion tv = t.getVersions().get(1);
-        double aziAnt = 0D;
-        TrajectorySegment tsAnt = null;
-        boolean first = true;
-        for (TrajectorySegment ts : tv.getSegments()) {
-            float lat1 = ts.getPoints().get(0).getLat();
-            float lgn1 = ts.getPoints().get(0).getLng();
-            float lat2 = ts.getPoints().get(1).getLat();
-            float lgn2 = ts.getPoints().get(1).getLng();
-            double azi = Azimuth.azimuth(lat1, lgn1, lat2, lgn2);
-            TrajectorySegmentData tsd = new TrajectorySegmentData();
-            tsd.setKey("azimuth");
-            tsd.setValue(String.valueOf(azi));
-            ts.addData(tsd);
-            if (!first) {
-                TrajectorySegmentData tsd2 = new TrajectorySegmentData();
-                tsd2.setKey("azimuthDiff");
-                tsd2.setValue(String.valueOf(Azimuth.azimuthDifference(azi, aziAnt)));
-                tsAnt.addData(tsd2);
+        for (long l : ll) {
+            Trajectory t = dao.findById(l);
+            TrajectoryVersion tv = t.getVersions().get(1);
+            double aziAnt = 0D;
+            TrajectorySegment tsAnt = null;
+            boolean first = true;
+            for (TrajectorySegment ts : tv.getSegments()) {
+                float lat1 = ts.getPoints().get(0).getLat();
+                float lgn1 = ts.getPoints().get(0).getLng();
+                float lat2 = ts.getPoints().get(1).getLat();
+                float lgn2 = ts.getPoints().get(1).getLng();
+                double azi = Azimuth.azimuth(lat1, lgn1, lat2, lgn2);
+                TrajectorySegmentData tsd = new TrajectorySegmentData();
+                tsd.setKey("azimuth");
+                tsd.setValue(String.valueOf(azi));
+                ts.addData(tsd);
+                if (!first) {
+                    TrajectorySegmentData tsd2 = new TrajectorySegmentData();
+                    tsd2.setKey("azimuthDiff");
+                    tsd2.setValue(String.valueOf(Azimuth.azimuthDifference(azi, aziAnt)));
+                    tsAnt.addData(tsd2);
+                    aziAnt = azi;
+                } else {
+                    first = false;
+                }
+                tsAnt = ts;
                 aziAnt = azi;
-            } else {
-                first = false;
             }
-            tsAnt = ts;
-            aziAnt = azi;
+            dao.store(t);
         }
-        dao.store(t);
     }
 }
--- trunk/src/main/java/br/udesc/mca/trajectory/segmenter/SegmenterExecuter.java
+++ trunk/src/main/java/br/udesc/mca/trajectory/segmenter/SegmenterExecuter.java
@@ -11,18 +11,21 @@ public class SegmenterExecuter {
 
     @SuppressWarnings("unchecked")
     public static void main(String[] args) throws Exception {
+        long[] ll = {20150418093055L, 20150419093055L};
         PersistenceDAO<Trajectory> dao = (PersistenceDAO<Trajectory>) PersistenceDAO
                 .getInstance(PersistenceModel.RELATIONAL);
-        Trajectory t = dao.findById(20150418093055L);
-        TrajectorySegment s = t.getVersions().get(0).getSegments().get(0);
-        SegmenterService ss = new SegmenterService();
-        List<TrajectorySegment> lts = ss.segmenter(s, 2);
-        TrajectoryVersion tv = new TrajectoryVersion();
-        tv.setVersion(2);
-        for (TrajectorySegment ts : lts) {
-            tv.addSegment(ts);
+        for (long l : ll) {
+            Trajectory t = dao.findById(l);
+            TrajectorySegment s = t.getVersions().get(0).getSegments().get(0);
+            SegmenterService ss = new SegmenterService();
+            List<TrajectorySegment> lts = ss.segmenter(s, 2);
+            TrajectoryVersion tv = new TrajectoryVersion();
+            tv.setVersion(2);
+            for (TrajectorySegment ts : lts) {
+                tv.addSegment(ts);
+            }
+            t.addVersion(tv);
+            dao.store(t);
         }
-        t.addVersion(tv);
-        dao.store(t);
     }
 }

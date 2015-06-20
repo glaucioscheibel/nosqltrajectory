11266d1d899cb697a8fcdf4ff6ffaf15f3d9e0b6
fhofmann01 <fhofmann01@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-06-20 19:59:26 +0000

    Versao teste dos mapeamentos

--- trunk/persistence-column/src/main/java/br/udesc/mca/trajectory/dao/column/CassandraPersistence.java
+++ trunk/persistence-column/src/main/java/br/udesc/mca/trajectory/dao/column/CassandraPersistence.java
@@ -3,42 +3,23 @@ package br.udesc.mca.trajectory.dao.column;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import br.udesc.mca.trajectory.dao.PersistenceDAO;
-import br.udesc.mca.trajectory.model.Trajectory;
-import com.datastax.driver.core.BoundStatement;
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ColumnDefinitions;
-import com.datastax.driver.core.ColumnMetadata;
-import com.datastax.driver.core.Host;
-import com.datastax.driver.core.KeyspaceMetadata;
-import com.datastax.driver.core.Metadata;
-import com.datastax.driver.core.PreparedStatement;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
-import com.datastax.driver.core.Session;
-import com.datastax.driver.core.TableMetadata;
+import br.udesc.mca.trajectory.model.*;
+import com.datastax.driver.core.*;
 
 public class CassandraPersistence extends PersistenceDAO {
+    private static final String space = "Trajectory";
+    private static final String table = "Trajectory";
+
     private static CassandraPersistence instance;
     private Cluster cluster;
     private Session db;
-    private String space;
-    private String table;
-    private String qname;
-    private Class type; // guarda a referencia prq recupera por reflection eh um
-                        // chute
 
     public static CassandraPersistence getInstance() {
-        return getInstance(Trajectory.class);
-    }
-
-    public static CassandraPersistence getInstance(Class type) {
         if (instance == null) {
-            System.out.println(CassandraPersistence.class);
             instance = new CassandraPersistence();
-            instance.type = type;
-            instance.createPersistenceTable(type);
         }
 
         return instance;
@@ -57,24 +38,41 @@ public class CassandraPersistence extends PersistenceDAO {
 
     @Override
     public void createDB() {
-        /*
-         * try { this.db.execute(
-         * "CREATE KEYSPACE dba WITH replication = {'class':'SimpleStrategy'," +
-         * " 'replication_factor':3};"); this.db.execute(
-         * "create table dba.customer (id int primary key, name text);");
-         * this.db.execute(
-         * "create table dba.customerdata (id int primary key, customerid int, key text, value text);"
-         * ); this.db.execute(
-         * "create index customerid on dba.customerdata (customerid)"); } catch
-         * (Exception e) { this.log.error(e.getMessage(), e); }
-         */
+        Metadata meta = this.cluster.getMetadata();
+        KeyspaceMetadata ks = null;
+        TableMetadata tb = null;
+        try {
+            ks = meta.getKeyspace(space);
+        } catch (Exception e) {}
+
+        if (ks != null) {
+            try {
+                tb = ks.getTable(table);
+            } catch (Exception e) {}
+        } else {
+            this.db.execute("CREATE KEYSPACE " + space + " WITH replication = {'class':'SimpleStrategy',"
+                    + " 'replication_factor':3};");
+        }
+        this.db.execute("use " +space);
+
+        if (tb == null) {
+            this.db.execute("CREATE TYPE data (id int, key text, value text)");
+           this.db.execute("CREATE TYPE user (id int, name text)");
+            this.db.execute("CREATE TYPE point (id int, h float, lat float, lng float, timestamp bigint, data list<frozen<data>>)");
+            this.db.execute("CREATE TYPE segment (transportationMode text, points list<frozen<point>>, data list<frozen<data>>)");
+            this.db.execute("CREATE TYPE process (componentId int, executionDuration bigint, executionTime bigint)");
+            this.db.execute("CREATE TYPE version (id int, lastModified timestamp, previousVersion int, version int, user map<frozen<user>>, segments list<frozen<segment>>, process list<frozen<segment>>)");
+
+            this.db.execute("create table " + table + " (id bigint primary key, lastModified timestamp, description text, originalTrajectory int, versions list<frozen<version>>)");
+
+        }
     }
 
     @Override
     public List<Trajectory> findAll() {
         List<Trajectory> ret = new ArrayList<>();
         try {
-            ResultSet rs = this.db.execute("select * from " + qname);
+            ResultSet rs = this.db.execute("select * from " + table);
             for (Row row : rs) {
                 ret.add(readRow(row));
             }
@@ -88,7 +86,7 @@ public class CassandraPersistence extends PersistenceDAO {
     public Trajectory findById(long id) {
         Trajectory ret = null;
         try {
-            PreparedStatement ps = this.db.prepare("select * from " + qname + " where id=?");
+            PreparedStatement ps = this.db.prepare("select * from " + table + " where id=?");
             BoundStatement bs = new BoundStatement(ps);
             bs.setLong(0, id);
             ResultSet rs = this.db.execute(bs);
@@ -106,7 +104,7 @@ public class CassandraPersistence extends PersistenceDAO {
     @Override
     public void deleteById(long id) {
         try {
-            PreparedStatement ps = this.db.prepare("delete from " + qname + " where id=?");
+            PreparedStatement ps = this.db.prepare("delete from " + table + " where id=?");
             BoundStatement bs = new BoundStatement(ps);
             bs.setLong(0, id);
             this.db.execute(bs);
@@ -124,154 +122,94 @@ public class CassandraPersistence extends PersistenceDAO {
         this.cluster = null;
     }
 
-    // ////////////////////////////TEstando metodos abaixo... estao longe de
-    // estar na versao final
     @Override
     public Trajectory store(Trajectory o) {
         StringBuilder sb = new StringBuilder();
-        StringBuilder values = new StringBuilder();
 
-        sb.append("insert into ").append(qname).append("(");
-        try {
-            boolean c = false;
-            TableMetadata tb = cluster.getMetadata().getKeyspace(space).getTable(table);
-            List<ColumnMetadata> cols = tb.getColumns();
+        Trajectory t = this.findById(o.getId());
+        if(t == null) {
+            //sb.append("insert into ").append(table).append("(id,lastModified,description,originalTrajectory,versions) values(?,?,?,?,[{id:1,version:1},{id:2,version:2}])");
+            sb.append("insert into ").append(table).append("(id,lastModified,description,originalTrajectory,versions) values(?,?,?,?,?)");
+            PreparedStatement st = this.db.prepare(sb.toString());
+            BoundStatement bs = st.bind();
+            bs.setLong("id", o.getId());
+            bs.setTimestamp("lastModified", o.getLastModified());
+            bs.setString("description", o.getDescription());
+            bs.setInt("originalTrajectory", o.getOriginalTrajectory());
 
-            for (ColumnMetadata col : cols) {
-                String getter = "get" + col.getName().substring(0, 1).toUpperCase() + col.getName().substring(1);
-                if (c) {
-                    sb.append(",");
-                    values.append(",");
-                }
-                sb.append(col.getName());
+            ArrayList<UDTValue> versions = new ArrayList<>();
+            UserType ut = db.getCluster()
+                    .getMetadata().getKeyspace("Trajectory").getUserType("version");
 
-                if ("TEXT".equals(col.getType().getName().name())) {
-                    values.append("'").append(type.getDeclaredMethod(getter).invoke(o)).append("'");
-                } else {
-                    values.append(type.getDeclaredMethod(getter).invoke(o));
-                }
+            for(TrajectoryVersion tv:o.getVersions()) {
+                UDTValue version = ut.newValue();
+                version.setInt("id", tv.getId());
+                version.setInt("version", tv.getVersion());
 
-                c = true;
+                versions.add(version);
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-            throw new RuntimeException(e.getMessage(), e);
+
+                bs.setList("versions", versions);
+
+            this.db.execute(bs);
+
+        } else {
+            sb.append("update ").append(table).append("");
         }
-
-        sb.append(") values (").append(values.toString()).append(")");
-        this.db.execute(sb.toString());
-
         return o;
     }
 
     private Trajectory readRow(Row row) throws Exception {
-        Object ret = type.newInstance();
+        Trajectory ret = new Trajectory();
         for (ColumnDefinitions.Definition def : row.getColumnDefinitions().asList()) {
-            String setter = "set" + def.getName().substring(0, 1).toUpperCase() + def.getName().substring(1);
-            Field f = type.getDeclaredField(def.getName());
-
-            if (Byte.class.equals(f.getType())) {
-                Integer v = row.getInt(f.getName());
-                if (v != null) {
-                    type.getDeclaredMethod(setter, Byte.class).invoke(ret, v.byteValue());
-                }
-            } else if (Short.class.equals(f.getType())) {
-                Integer v = row.getInt(f.getName());
-                if (v != null) {
-                    type.getDeclaredMethod(setter, Short.class).invoke(ret, v.shortValue());
-                }
-            } else if (Integer.class.equals(f.getType())) {
-                type.getDeclaredMethod(setter, Integer.class).invoke(ret, row.getInt(f.getName()));
-            } else if ("int".equals(f.getType().getName())) {
-                type.getDeclaredMethod(setter, Integer.TYPE).invoke(ret, row.getInt(f.getName()));
-            } else if (Long.class.equals(f.getType())) {
-                type.getDeclaredMethod(setter, Long.class).invoke(ret, row.getLong(f.getName()));
-            } else if ("long".equals(f.getType().getName())) {
-                type.getDeclaredMethod(setter, Long.TYPE).invoke(ret, row.getLong(f.getName()));
-            } else if (Float.class.equals(f.getType())) {
-                type.getDeclaredMethod(setter, Float.class).invoke(ret, row.getFloat(f.getName()));
-            } else if (Double.class.equals(f.getType())) {
-                type.getDeclaredMethod(setter, Double.class).invoke(ret, row.getDouble(f.getName()));
-            } else if (String.class.equals(f.getType())) {
-                type.getDeclaredMethod(setter, String.class).invoke(ret, row.getString(f.getName()));
+            ret.setId(row.getLong("id"));
+            ret.setDescription(row.getString("description"));
+            ret.setOriginalTrajectory(row.getInt("originalTrajectory"));
+            ret.setLastModified(row.getTimestamp("lastModified"));
+            List<UDTValue> versions = row.getList("versions", UDTValue.class);
+            for(UDTValue version:versions){
+                TrajectoryVersion v = new TrajectoryVersion();
+                v.setId(version.getInt("id"));
+                v.setPreviousVersion(version.getInt("previousVersion"));
+                v.setVersion(version.getInt("version"));
+                v.setLastModified(version.getTimestamp("lastModified"));
+                //id int, lastModified timestamp, previousVersion int, version int, user map<text, frozen<user>>, segments set<frozen<segment>>, process map<text, frozen<segment>>
+                ret.addVersion(v);
             }
         }
 
-        return (Trajectory) ret;
+        return ret;
     }
 
-    /**
-     * Cria a tabela baseados nos atributos de uma classe (obrigatorio a classe
-     * ter um atributo long id)... atualmente soh persiste primitivos e string
-     * 
-     * @param
-     */
-    private void createPersistenceTable(Class klass) {
-        String name = klass.getName();
-        space = name.substring(0, name.lastIndexOf('.')).replace('.', 'x');
-        table = klass.getName().substring(name.lastIndexOf('.') + 1);
-        qname = space + "." + table;
-        Metadata meta = this.cluster.getMetadata();
-        KeyspaceMetadata ks = null;
-        TableMetadata tb = null;
+    public  static void main(String... a){
+        CassandraPersistence p = CassandraPersistence.getInstance();
         try {
-            ks = meta.getKeyspace(space);
-        } catch (Exception e) {}
 
-        if (ks != null) {
-            try {
-                tb = ks.getTable(table);
-            } catch (Exception e) {}
-        } else {
-            this.db.execute("CREATE KEYSPACE " + space + " WITH replication = {'class':'SimpleStrategy',"
-                    + " 'replication_factor':3};");
-        }
+            p.createDB();
 
-        if (tb == null) {
-            StringBuilder sb = new StringBuilder();
-            sb.append("create table ").append(qname).append(" (id bigint primary key");
-            Field[] fs = klass.getDeclaredFields();
+            Trajectory t = new Trajectory();
 
-            for (Field f : fs) {
-                if (Modifier.isStatic(f.getModifiers())
-                        || (!f.getType().isPrimitive() && !String.class.equals(f.getType()))) {// desconsiderar
-                                                                                               // staticos
-                                                                                               // e
-                                                                                               // nao
-                                                                                               // primitivos
-                                                                                               // por
-                                                                                               // enquanto
-                    continue;
-                }
-                switch (f.getName()) {
-                case "id":
-                case "serialversion":
-                    // ignorados
-                    break;
-                default:
+            t.setId(2l);
+            t.setDescription("desc");
+            t.setLastModified(new Date());
+            t.setOriginalTrajectory(0);
 
-                    if (Byte.class.equals(f.getType())) {
-                        sb.append(",").append(f.getName()).append(" int ");
-                    } else if (Short.class.equals(f.getType())) {
-                        sb.append(",").append(f.getName()).append(" int ");
-                    } else if (Integer.class.equals(f.getType())) {
-                        sb.append(",").append(f.getName()).append(" int ");
-                    } else if (Long.class.equals(f.getType())) {
-                        sb.append(",").append(f.getName()).append(" bigint ");
-                    } else if (Float.class.equals(f.getType())) {
-                        sb.append(",").append(f.getName()).append(" float ");
-                    } else if (Double.class.equals(f.getType())) {
-                        sb.append(",").append(f.getName()).append(" double ");
-                    } else if (String.class.equals(f.getType())) {
-                        sb.append(",").append(f.getName()).append(" text ");
-                    }
 
-                }
-            }
-            sb.append(")");
-            this.db.execute(sb.toString());
-            // this.db.execute("create index "+table+"id on "+qname+"(id)"); PK
-            // jah eh indice
+            TrajectoryVersion v = new TrajectoryVersion();
+            v.setId(1);
+            v.setVersion(1);
+            t.addVersion(v);
+            p.store(t);
+
+
+            System.out.println(p.findById(2).getDescription());
+            System.out.println(p.findById(2).getVersions().get(0).getId());
+
+
+        }catch (Exception e){
+            e.printStackTrace();
+        } finally {
+            p.close();
         }
     }
 }

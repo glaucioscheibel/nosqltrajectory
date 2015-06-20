9039689e6018a82afc6e878b1f00ed8eeb28b8b0
fhofmann01 <fhofmanns@users.noreply.github.com>
2015-06-20 20:13:48 +0000

    Padronizado

--- persistence-column/src/main/java/br/udesc/mca/trajectory/dao/column/ColumnPersistence.java
+++ persistence-column/src/main/java/br/udesc/mca/trajectory/dao/column/ColumnPersistence.java
@@ -3,12 +3,9 @@ package br.udesc.mca.trajectory.dao.column;
 import br.udesc.mca.trajectory.dao.PersistenceDAO;
 import br.udesc.mca.trajectory.dao.PersistenceModel;
 
-public abstract class ColumnPersistence {
-    public static PersistenceDAO getInstance() {
-        return PersistenceDAO.getInstance(PersistenceModel.COLUMN_FAMILY);
+public abstract class ColumnPersistence  extends PersistenceDAO {
+    public static ColumnPersistence getInstance() {
+        return (ColumnPersistence) PersistenceDAO.getInstance(PersistenceModel.COLUMN_FAMILY);
     }
 
-    public static PersistenceDAO getInstance(Class klass) {
-        return PersistenceDAO.getInstance(PersistenceModel.COLUMN_FAMILY, klass);
-    }
 }

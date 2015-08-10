31b183b88f401614ca197c1f82bd37300564f6fd
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-08-10 01:24:50 +0000

    - Adição de comentário sobre a questão do modelo.

--- importertraj/src/main/java/br/udesc/mca/conexao/GeraModelo.java
+++ importertraj/src/main/java/br/udesc/mca/conexao/GeraModelo.java
@@ -2,12 +2,15 @@ package br.udesc.mca.conexao;
 
 import org.hibernate.Session;
 
+//lembrar antes de executar o gerador do modelo,
+//de criar a extensão espacial no banco alvo.
+//PostgreSQL = CREATE EXTENSION postgis;
 public class GeraModelo {
 
 	public static void main(String[] args) {
 		Session sessao = null;
 		sessao = HibernateUtil.getSessionFactory().openSession();
-		System.out.println("Conectou!");
-		sessao.close();		
+		System.out.println("Conectou e gerou o modelo!");
+		sessao.close();
 	}
 }

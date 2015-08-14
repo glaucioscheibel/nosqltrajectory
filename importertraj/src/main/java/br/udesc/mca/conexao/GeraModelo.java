7e6855143f89b1104e246678aae3d56f0cbebd75
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-08-14 01:19:34 +0000

    - Java doc da classe adicionado.

--- importertraj/src/main/java/br/udesc/mca/conexao/GeraModelo.java
+++ importertraj/src/main/java/br/udesc/mca/conexao/GeraModelo.java
@@ -4,7 +4,14 @@ import org.hibernate.Session;
 
 //lembrar antes de executar o gerador do modelo,
 //de criar a extensão espacial no banco alvo.
+//Exemplo a seguir no PostgreSQL
 //PostgreSQL = CREATE EXTENSION postgis;
+
+/**
+ * Classe responsável por gerar o modelo de trajetórias.
+ * 
+ * @since 25/12/2012
+ */
 public class GeraModelo {
 
 	public static void main(String[] args) {

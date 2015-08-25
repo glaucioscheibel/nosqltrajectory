a6599248a4751d37eb690878c0a6607f7cc2cba7
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-08-25 20:30:38 +0000

    - Adição do cálculo de distância perpendicular.

--- trunk/importetraj/src/main/java/br/udesc/mca/matematica/Azimute.java
+++ trunk/importetraj/src/main/java/br/udesc/mca/matematica/Azimute.java
@@ -1,18 +1,21 @@
 package br.udesc.mca.matematica;
 
+import br.udesc.mca.modelo.ponto.Ponto;
+
 /**
  * Classe que trabalha com a questão do cálculo de Azimute e Distância em KM.
- * Com base no trabalho em: http://www.movable-type.co.uk/scripts/latlong.html.
+ * Com base no trabalho em: http://www.movable-type.co.uk/scripts/latlong.html e
+ * http://robbyn.github.io/hiketools/
  * 
  * @since 25/12/2014
  */
 public final class Azimute {
-	
+
 	/**
 	 * Valor do raio de curvatura de terra em KM
 	 */
 	public static int RAIO_TERRA_KM = 6371;
-
+	
 	/**
 	 * Retorna a distância entre dois pontos usando a fórmula de haversine.
 	 * 
@@ -27,20 +30,63 @@ public final class Azimute {
 	 * 
 	 * @returns Distância em KM entre os dois pontos.
 	 */
-	public static double calculaDistanciaKM(double lat1, double lon1,
-			double lat2, double lon2) {
+	public static double calculaDistanciaKM(double lat1, double lon1, double lat2, double lon2) {
 
 		double latDistance = Math.toRadians(lat2 - lat1);
 		double lonDistance = Math.toRadians(lon2 - lon1);
 
-		double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
-				+ Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
-				* Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+		double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2) + Math.cos(Math.toRadians(lat1))
+				* Math.cos(Math.toRadians(lat2)) * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
 
 		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
 		double distance = Azimute.RAIO_TERRA_KM * c;
 
 		return distance;
+	}
+
+	/**
+	 * Calcula a distância a partir de um ponto P passando pelo grande círculo
+	 * que passa por outros dois pontos A e B.
+	 * 
+	 * @param p
+	 *            ponto
+	 * @param a
+	 *            primeiro ponto
+	 * @param b
+	 *            segundo ponto
+	 * @return distância, em metros
+	 */
+	public static double distanciaPerpendicular(Ponto p, Ponto a, Ponto b) {
+		double lata = Math.toRadians(a.getLatitude());
+		double lnga = Math.toRadians(a.getLongitude());
+		double latb = Math.toRadians(b.getLatitude());
+		double lngb = Math.toRadians(b.getLongitude());
+		double latp = Math.toRadians(p.getLatitude());
+		double lngp = Math.toRadians(p.getLongitude());
+		double sinlata = Math.sin(lata);
+		double coslata = Math.cos(lata);
+		double sinlnga = Math.sin(lnga);
+		double coslnga = Math.cos(lnga);
+		double sinlatb = Math.sin(latb);
+		double coslatb = Math.cos(latb);
+		double sinlngb = Math.sin(lngb);
+		double coslngb = Math.cos(lngb);
+		double sinlatp = Math.sin(latp);
+		double coslatp = Math.cos(latp);
+		double sinlngp = Math.sin(lngp);
+		double coslngp = Math.cos(lngp);
+		double costh = sinlata * sinlatb + coslata * coslatb * (coslnga * coslngb + sinlnga * sinlngb);
+		double sin2th = 1 - costh * costh;
+		if (sin2th < 1.0E-8) {
+			// a e b são muito próximos; return a distância de a para p
+			double costhp = sinlata * sinlatp + coslata * coslatp * (coslnga * coslngp + sinlnga * sinlngp);
+			return Math.acos(costhp) * (RAIO_TERRA_KM + p.getAltitude());
+		}
+		double num = sinlata * (coslatb * coslatp * coslngb * sinlngp - coslatb * coslatp * sinlngb * coslngp)
+				+ coslata * coslnga * (coslatb * sinlatp * sinlngb - sinlatb * coslatp * sinlngp)
+				+ coslata * sinlnga * (sinlatb * coslatp * coslngp - coslatb * sinlatp * coslngb);
+		double sinr = Math.abs(num) / Math.sqrt(sin2th);
+		return (RAIO_TERRA_KM + p.getAltitude()) * Math.asin(sinr);
 	}
 
 	/**
@@ -56,8 +102,7 @@ public final class Azimute {
 	 *            logintude do segundo ponto
 	 * @returns azimute em graus.
 	 */
-	public static double azimute(double lat1, double lon1, double lat2,
-			double lon2) {
+	public static double azimute(double lat1, double lon1, double lat2, double lon2) {
 		double phi1 = Math.toRadians(lat1);
 		double phi2 = Math.toRadians(lat2);
 		double delta = Math.toRadians(lon2 - lon1);
@@ -80,5 +125,4 @@ public final class Azimute {
 	public static double diferencaAzimute(double azimute1, double azimute2) {
 		return azimute1 - azimute2;
 	}
-
 }

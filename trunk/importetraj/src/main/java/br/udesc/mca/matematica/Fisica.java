38087998eca709a0e4df177e7c5acfd322f5f6ab
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-08-25 20:31:07 +0000

    - Cálculo de velocidade média e distância euclidiana sincronizada.

--- trunk/importetraj/src/main/java/br/udesc/mca/matematica/Fisica.java
+++ trunk/importetraj/src/main/java/br/udesc/mca/matematica/Fisica.java
@@ -1,5 +1,7 @@
 package br.udesc.mca.matematica;
 
+import br.udesc.mca.modelo.ponto.Ponto;
+
 /**
  * Classe que trabalha com a questão do cálculo de Azimute e Distância em KM.
  * Com base no trabalho em: http://www.movable-type.co.uk/scripts/latlong.html.
@@ -7,6 +9,11 @@ package br.udesc.mca.matematica;
  * @since 25/12/2014
  */
 public class Fisica {
+
+	/**
+	 * Valor da distância euclidiana sincronizada
+	 */
+	public static final int SED = 1;
 
 	/**
 	 * Método para calcular a velocidade média. Os parâmetros passados devem
@@ -41,11 +48,11 @@ public class Fisica {
 
 	/**
 	 * Método para calcular a velocidade média. Os parâmetros passados devem
-	 * estar em metros e segundos, conforme o . 
+	 * estar em metros e segundos, conforme o .
 	 * 
 	 * @param distanciaPercorrida
 	 * @param duracao
-
+	 * 
 	 * @return A velocidade média.
 	 */
 	public static final double velocidadeMediaSistemaInternacional(double distanciaPercorrida, double duracao) {
@@ -56,4 +63,73 @@ public class Fisica {
 		}
 		return vm;
 	}
+
+	/**
+	 * Retorna a velocidade média entre dois pontos
+	 * 
+	 * @param pontoA
+	 * @param pontoB
+	 * @return A velocidade em metros/segundo
+	 */
+	public static double velocidadeMediaSistemaInternacionalPonto(Ponto pontoA, Ponto pontoB) {
+		double vm = 0;
+
+		double tempo = pontoB.getTempo().getTime() - pontoA.getTempo().getTime();
+
+		if (tempo == 0) {
+			return 0;
+		}
+
+		tempo = tempo / 1000; // Milissegundos para Segundos
+		double distancia = Azimute.calculaDistanciaKM(pontoA.getLatitude(), pontoA.getLongitude(), pontoB.getLatitude(),
+				pontoB.getLongitude());
+
+		vm = distancia / tempo;
+		return vm;
+	}
+
+	/**
+	 * Método que cálcula a distância euclidiana sincronizada. Com base na
+	 * fórmula:
+	 * 
+	 * sed(A,B,C) = square root of ((x'B - xB)*(x'B - xB) + ((y'B - yB)*(y'B -
+	 * yB)) x'B = xA + vXAC * (tb - ta) y'B = yA + vYAC * (tb - ta) vXAC = (xc -
+	 * xa) / (tc - ta) vYAC = (yc - ya) / (tc - ta)
+	 * 
+	 * @param ponto
+	 *            (p)
+	 * @param a
+	 *            (a)
+	 * @param b
+	 *            (a)
+	 * 
+	 * @return a distância euclidiana sincronizada
+	 */
+	public static double getSED(Ponto p, Ponto a, Ponto b) {
+
+		double timeRatioA = b.getTempo().getTime() - a.getTempo().getTime();
+
+		if (timeRatioA == 0) {
+			return 0;
+		}
+
+		double velocityVectorX = (b.getLongitude() - a.getLongitude()) / (timeRatioA);
+
+		double velocityVectorY = (b.getLatitude() - a.getLatitude()) / (timeRatioA);
+
+		double predictedX = a.getLongitude() + velocityVectorX * (p.getTempo().getTime() - a.getTempo().getTime());
+
+		double predictedY = a.getLatitude() + velocityVectorY * (p.getTempo().getTime() - a.getTempo().getTime());
+
+		Ponto predicted = new Ponto();
+
+		predicted.setLatitude(predictedY);
+		predicted.setLongitude(predictedX);
+
+		double sed = Azimute.calculaDistanciaKM(p.getLatitude(), p.getLongitude(), predicted.getLatitude(),
+				predicted.getLongitude());
+
+		return sed;
+	}
+
 }

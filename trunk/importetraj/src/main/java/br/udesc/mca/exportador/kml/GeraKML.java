a1f548412b003488ffe6921a55a96f246f43d0d0
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-08-30 00:07:18 +0000

    - Adicionado o comando para fechar a conexão do hibernate.

--- trunk/importetraj/src/main/java/br/udesc/mca/exportador/kml/GeraKML.java
+++ trunk/importetraj/src/main/java/br/udesc/mca/exportador/kml/GeraKML.java
@@ -79,6 +79,7 @@ public class GeraKML {
 			// kml.setFeature(lugar);
 			try {
 				kml.marshal(new File(trajetoria.getArquivo() + ".kml"));
+				sessao.close();
 			} catch (FileNotFoundException e) {
 				e.printStackTrace();
 			}

5cfffb2c756e844a06be10b4ae61c415701dfb6f
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2015-08-30 00:06:41 +0000

    - Implementação parcial da consulta para segmento.

--- trunk/importetraj/src/main/java/br/udesc/mca/calculador/DiferencaAzimute.java
+++ trunk/importetraj/src/main/java/br/udesc/mca/calculador/DiferencaAzimute.java
@@ -1,9 +1,26 @@
 package br.udesc.mca.calculador;
 
+import java.math.BigInteger;
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+
+import br.udesc.mca.conexao.HibernateUtil;
+import br.udesc.mca.modelo.segmento.Segmento;
+
 public class DiferencaAzimute {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+
+		Query consulta = sessao.getNamedQuery("consultaSegmentoTrajetoria");
+		consulta.setBigInteger("trajetoriaId", new BigInteger("1"));
+		List<Segmento> resultado = consulta.list();
+
+		for (Segmento segmento : resultado) {
+			System.out.println("Teste: " + segmento.getId() + "-" + segmento.getAzimute());
+		}
 
 	}
 

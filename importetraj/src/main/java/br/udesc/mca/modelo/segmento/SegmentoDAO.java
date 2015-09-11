6e5e7598725e29a606a8f10edd7f3f97e16185c7
Alexandre <am2net@users.noreply.github.com>
2015-09-11 17:59:20 +0000

    - Alteração para o segmento.

--- importetraj/src/main/java/br/udesc/mca/modelo/segmento/SegmentoDAO.java
+++ importetraj/src/main/java/br/udesc/mca/modelo/segmento/SegmentoDAO.java
@@ -4,15 +4,13 @@ import java.util.Collection;
 
 import javax.sql.RowSet;
 
-import br.udesc.mca.modelo.ponto.Ponto;
-
 public interface SegmentoDAO {
 
 	public int inserirSegmento(Segmento segmento);
 
 	public boolean excluirSegmento(long id);
 
-	public Ponto selecionarSegmento(long id);
+	public Segmento selecionarSegmento(long id);
 
 	public boolean atualizarSegmento(Segmento segmento);
 

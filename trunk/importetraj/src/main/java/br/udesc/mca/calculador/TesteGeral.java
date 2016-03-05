560f62586f49de1d8e036031027fe70e278ed04a
am2net <am2net@6027d77a-0421-43a9-82e4-94cb8f093177>
2016-03-05 19:53:00 +0000

    - Adicionado linhas para imprimir outros resultados.

--- trunk/importetraj/src/main/java/br/udesc/mca/calculador/TesteGeral.java
+++ trunk/importetraj/src/main/java/br/udesc/mca/calculador/TesteGeral.java
@@ -5,9 +5,13 @@ import br.udesc.mca.matematica.Azimute;
 public class TesteGeral {
 
 	public static void main(String[] args) throws Exception {
-		double distancia1 = Azimute.calculaDistanciaKM(-26.230674, -48.916587, -26.212370, -48.915916);
-		
-		System.out.println(Math.round(distancia1*1000));
+		double distancia = Azimute.calculaDistanciaKM(-22.910547, -43.269951, -22.9105, -43.269966);
+		double distancia1 = Azimute.calculaDistanciaMetros(-22.910547, -43.269951, -22.9105, -43.269966, false);
+
+		System.out.println(Math.round(distancia));
+		System.out.println(Math.round(distancia * 1000));
+		System.out.println(Math.round(distancia1));
+		System.out.println(Math.round(distancia1 * 1000));
 
 	}
 

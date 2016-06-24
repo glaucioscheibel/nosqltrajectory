812dbb013a772bd23d808e497da62e70c7e25495
Glaucio Scheibel <gscheibel@gmail.com>
2016-06-24 16:00:58 -0300

    Trocado o banco MultiModelo para o ArangoDB

--- persistence-multimodel/src/main/java/br/udesc/mca/trajectory/dao/multimodel/ArangoDBPersistence.java
+++ persistence-multimodel/src/main/java/br/udesc/mca/trajectory/dao/multimodel/ArangoDBPersistence.java
@@ -3,17 +3,17 @@ package br.udesc.mca.trajectory.dao.multimodel;
 import java.util.List;
 import br.udesc.mca.trajectory.model.Trajectory;
 
-public class OrientDBPersistence extends MultiModelPersistence {
-    private static OrientDBPersistence instance;
+public class ArangoDBPersistence extends MultiModelPersistence {
+    private static ArangoDBPersistence instance;
     
-    public static OrientDBPersistence getInstance() {
+    public static ArangoDBPersistence getInstance() {
         if (instance == null) {
-            instance = new OrientDBPersistence();
+            instance = new ArangoDBPersistence();
         }
         return instance;
     }
 
-    private OrientDBPersistence() {}
+    private ArangoDBPersistence() {}
 
     @Override
     public Trajectory store(Trajectory t) {
